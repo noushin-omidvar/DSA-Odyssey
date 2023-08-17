c565433ff7fbd4733f5382c19e0c85a08a598932
Noushin Omidvar <noush.omidvar@gmail.com>
2023-08-16 17:15:43 -0700

    updated

--- arrays/3-URLify/solution.py
+++ arrays/3-URLify/solution.py
@@ -1,12 +1,12 @@
 def urlify(s, true_length):
     s = list(s)  # Convert the input string to a list of characters
-    space_count = s[:true_length].count(' ')  # Count the spaces within the true length of the string
-    index = true_length + space_count * 2  # Calculate the new length after URL encoding
+    # Count the spaces within the true length of the string
+    space_count = s[:true_length].count(' ')
+    # Calculate the new length after URL encoding
+    index = true_length + space_count * 2
 
-    if true_length < len(s) :
-        s[true_length] = '\0'
-
-    for i in range(true_length - 1, -1, -1):  # Iterate through the characters in reverse order
+    # Iterate through the characters in reverse order
+    for i in range(true_length - 1, -1, -1):
         if s[i] == ' ':
             # Replace a space with '%20' and adjust the index for the next character
             s[index - 1] = '0'
@@ -17,9 +17,10 @@ def urlify(s, true_length):
             # Copy non-space character to its new position and adjust the index
             s[index - 1] = s[i]
             index -= 1
-    
+
     # Convert the modified character list back to a string and return
     return ''.join(s)
+
 
 # Test cases
 input1 = "Mr John Smith    "
